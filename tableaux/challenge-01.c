dd364b64819f0bf243da6b208d0fcbd81777b374
imran-one <benmiloudimran@gmail.com>
2025-09-08 15:39:38 +0200

    semple tableau

---
+++ tableaux/challenge-01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int tableau[5]={1,2,3,4,5};
+
+    for (i=0; i<5;i++){
+        printf("la valeur du tableau[%d] est%d",i,tableau[i]);
+
+    }
+
+    return 0;
+
+}
