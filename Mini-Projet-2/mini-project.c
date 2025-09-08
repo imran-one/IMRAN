c03bdd7aa3da937012c765352cf9ca6b252565c5
imran-one <benmiloudimran@gmail.com>
2025-09-08 11:52:02 +0200

    mini-project

---
+++ Mini-Projet-2/mini-project.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+    int len=6;
+    int i=6;
+
+    char tableau[len]={'h','e','l','l','o','\o'};
+    char tableau[i]={'_','_','_','_','_','\o'};
+
+
+    return 0;
+
+}
