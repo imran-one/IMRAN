67aaa66995e4ba49b32d9b673beecbab56b1556f
imran-one <benmiloudimran@gmail.com>
2025-09-11 10:08:43 +0200

    Update challenge-2

--- Chaînes-de-Caractères/challenge-2.c
+++ Chaînes-de-Caractères/challenge-2.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
 
 int main(){
-    char tab[]="IMRANE";
+    char x[100];
+    printf("entre une phrase : ");
+    fgets(x,100,stdin);
     int r=0;
-
-    while (tab[r]!='\0'){
-        r++;
+    while (x[r]!='\0'){
+         r++;
     }
-    printf("%d ",r);
+    printf("\n%d",r-1);
+    
     return 0;
 }
