20f18924b99c161148dda717f3e359b89c0667eb
imran-one <benmiloudimran@gmail.com>
2025-09-11 12:35:55 +0200

    Suppresseur d'Espaces

---
+++ Chaînes-de-Caractères/challenge-9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char tab1[100];
+    char tab2[100];
+    int j = 0;
+
+    
+    printf("Entrez une phrase  : ");
+    fgets(tab1, 100, stdin);
+
+    for (int i = 0; tab1[i] != '\0'; i++) {
+        if (tab1[i] != ' ') {
+            tab2[j] = tab1[i];
+            j++;
+        }
+    }
+    
+    tab2[j] = '\0';
+
+    printf("Chaine sans espaces : %s\n", tab2);
+
+    return 0;
+}
